e232d24f2ab57533c5ad4d19d66d265cfe91eef7
Ha1seZz <fahretdinovdamir7@gmail.com>
2026-01-28 21:44:09 +0500

    initialize core config with pydantic-settings

---
+++ core/config.py
@@ -0,0 +1,46 @@
+from pathlib import Path
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+BASE_DIR = Path(__file__).parent.parent  # Определяем путь к корневой директории проекта
+
+
+class DbSettings(BaseModel):
+    """
+    Схема настроек базы данных.
+    """
+    host: str = "localhost"
+    port: int = 5432
+    user: str = "postgres"
+    password: str = "password"
+    name: str = "database"
+    echo: bool = False
+
+    @property
+    def database_url(self) -> str:
+        """
+        Собирает DSN строку для SQLAlchemy (asyncpg).
+        """
+        return f"postgresql+asyncpg://{self.user}:{self.password}@{self.host}:{self.port}/{self.name}"
+
+
+class Settings(BaseSettings):
+    """
+    Основной класс настроек приложения. Наследуется от BaseSettings,
+    что дает возможность автоматически читать данные из переменных окружения и .env файлов.
+    """
+    model_config = SettingsConfigDict(
+        env_file=BASE_DIR / ".env",
+        env_file_encoding="utf-8",
+        case_sensitive=False,
+        extra="ignore",
+        env_nested_delimiter="__"
+    )
+
+    api_v1_prefix: str = "/api/v1"
+
+    db: DbSettings = DbSettings()
+
+
+settings = Settings()
