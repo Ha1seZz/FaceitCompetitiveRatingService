f920b114abd0ffd35d3eab2adbcc3c3340e5f438
Ha1seZz <fahretdinovdamir7@gmail.com>
2026-01-28 22:24:54 +0500

    add DatabaseHelper with async session and scoped session support

---
+++ core/models.py/db_helper.py
@@ -0,0 +1,57 @@
+from asyncio import current_task
+
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    create_async_engine,
+    async_sessionmaker,
+    async_scoped_session,
+)
+
+from core.config import settings
+
+
+class DatabaseHelper:
+    """
+    Класс-помощник для управления подключением к БД и сессиями.
+    Инкапсулирует создание движка (engine) и фабрики сессий.
+    """
+    def __init__(self, url: str, echo: bool = False):
+        self.engine = create_async_engine(url=url, echo=echo)  # Создаем асинхронный движок
+        self.session_factory = async_sessionmaker(  # Фабрика сессий
+            bind=self.engine,
+            autoflush=False,
+            autocommit=False,
+            expire_on_commit=False,
+        )
+
+    def get_scoped_session(self):
+        """
+        Создает сессию, ограниченную текущей задачей asyncio (current_task).
+        """
+        session = async_scoped_session(
+            session_factory=self.session_factory,
+            scopefunc=current_task,
+        )
+        return session
+
+    async def session_dependency(self) -> AsyncSession:  # type: ignore
+        """
+        Генератор сессий для FastAPI Depends.
+        """
+        async with self.session_factory() as session:
+            yield session
+            await session.close()
+
+    async def scoped_session_dependency(self) -> AsyncSession:  # type: ignore
+        """
+        Альтернативный генератор для использования scoped сессий.
+        """
+        session = self.get_scoped_session()
+        yield session
+        await session.remove()
+
+
+db_helher = DatabaseHelper(
+    url=settings.db.url,
+    echo=settings.db.echo,
+)
