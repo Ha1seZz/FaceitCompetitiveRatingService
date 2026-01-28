9156bd43d5bbb99f5dc1ad44173316f3f8d88ef0
Ha1seZz <fahretdinovdamir7@gmail.com>
2026-01-28 22:08:12 +0500

    add declarative base model with automated tablename and id

---
+++ core/models.py/base.py
@@ -0,0 +1,20 @@
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import declared_attr
+
+from core.config import settings
+
+
+class Base(DeclarativeBase):
+    """
+    Базовый класс для всех моделей SQLAlchemy.
+    Использует DeclarativeBase (стандарт SQLAlchemy 2.0+).
+    """
+    __abstract__ = True
+
+    @declared_attr.directive
+    def __tablename__(cls) -> str:
+        return f"{cls.__name__.lower()}s"  # Player -> players
+
+    id: Mapped[int] = mapped_column(primary_key=True)  # Первичный ключ для всех моделей
