a64d7706d18e66d90a3d84bfb8a95aff8c8d5227
Ha1seZz <fahretdinovdamir7@gmail.com>
2026-01-29 23:09:52 +0500

    feat(crud): implement create_or_update_player function for player management

---
+++ app/api/api_v1/players/crud.py
@@ -0,0 +1,27 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from models.player import Player
+from .schemas import PlayerCreate
+
+
+async def create_or_update_player(
+    session: AsyncSession,
+    player_in: PlayerCreate,
+) -> Player:
+    stmt = select(Player).where(Player.player_id == player_in.player_id)
+    result = await session.execute(stmt)
+    db_player = result.scalar_one_or_none()
+
+    player_data = player_in.model_dump()
+
+    if db_player:  # if exists - update
+        for key, value in player_data.items():
+            setattr(db_player, key, value)
+    else:  # if not exists - create
+        db_player = Player(**player_data)
+        session.add(db_player)
+
+    await session.commit()
+    await session.refresh()
+    return db_player
