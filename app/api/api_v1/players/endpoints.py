6b4082960b7c3f12ca2849953a1b018ed145854b
Ha1seZz <fahretdinovdamir7@gmail.com>
2026-01-29 19:34:35 +0500

    feat(api): implement player profile retrieval endpoint

--- app/api/api_v1/players/endpoints.py
+++ app/api/api_v1/players/endpoints.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, HTTPException, status
 
-from .schemas import PlayerSchema
+from .services.faceit_client import FaceitClient
+from .schemas import PlayerDetails
 from core.config import settings
 
 
@@ -8,3 +9,17 @@ router = APIRouter(
     prefix=settings.api.v1.players,  # /players
     tags=["Players"],
 )
+
+
+@router.get("/{nickname}", response_model=PlayerDetails)
+async def get_player_profile(nickname: str):
+    """Получает информацию об игроке из Faceit."""
+    player_details = await FaceitClient().get_player_details(nickname=nickname)
+
+    if not player_details:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Player not found on Faceit",
+        )
+
+    return player_details
