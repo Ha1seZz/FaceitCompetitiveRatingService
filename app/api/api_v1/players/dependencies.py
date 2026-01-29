b3c78fabebee77be62a8385fdf3e096d896884f2
Ha1seZz <fahretdinovdamir7@gmail.com>
2026-01-29 20:26:27 +0500

    feat(api): add the dependency of getting a Faceit player by nickname

---
+++ app/api/api_v1/players/dependencies.py
@@ -0,0 +1,11 @@
+from fastapi import HTTPException, status
+from .services.faceit_client import FaceitClient
+
+async def get_current_faceit_player(nickname: str) -> dict:
+    player_data = await FaceitClient().get_player_details(nickname=nickname)
+    if not player_data:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Player not found on Faceit",
+        )
+    return player_data
