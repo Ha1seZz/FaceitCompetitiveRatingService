55c160de925f8b61561ff97e2287561e5a11f81a
Ha1seZz <fahretdinovdamir7@gmail.com>
2026-01-30 01:48:10 +0500

    fix(crud): resolve TypeError by filtering API data through Pydantic

--- app/api/api_v1/players/crud.py
+++ app/api/api_v1/players/crud.py
@@ -7,21 +7,26 @@ from .schemas import PlayerCreate
 
 async def create_or_update_player(
     session: AsyncSession,
-    player_in: PlayerCreate,
+    player_in: dict,
 ) -> Player:
-    stmt = select(Player).where(Player.player_id == player_in.player_id)
+    """
+    Создает нового игрока или обновляет существующего на основе данных из Faceit API.
+    """
+    # Отсекает лишние поля и преобразует в словарь
+    validated_data = PlayerCreate(**player_in)
+    player_data = validated_data.model_dump()
+
+    stmt = select(Player).where(Player.player_id == player_data.get("player_id", ""))
     result = await session.execute(stmt)
     db_player = result.scalar_one_or_none()
 
-    player_data = player_in.model_dump()
-
-    if db_player:  # if exists - update
+    if db_player:  # if player exists - update
         for key, value in player_data.items():
             setattr(db_player, key, value)
-    else:  # if not exists - create
+    else:  # if player not exists - create
         db_player = Player(**player_data)
         session.add(db_player)
 
     await session.commit()
-    await session.refresh()
+    await session.refresh(db_player)
     return db_player
