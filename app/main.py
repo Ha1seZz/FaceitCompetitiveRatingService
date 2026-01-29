23b83aa8361909937635ba92ebf7dca3af088315
Ha1seZz <fahretdinovdamir7@gmail.com>
2026-01-29 18:57:08 +0500

    refactor(main): clean up imports and enhance error handling for external API calls

--- app/main.py
+++ app/main.py
@@ -1,16 +1,20 @@
-from contextlib import asynccontextmanager
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+import httpx
 import uvicorn
-
-from core.config import settings
 from api import router as api_router  # Импорт основного роутера API v1
 
 
-app = FastAPI(
-    title="Faceit Competitive Rating Service"
-)
+app = FastAPI(title="Faceit Competitive Rating Service")
 app.include_router(api_router)
+
+
+@app.exception_handler(httpx.HTTPStatusError)
+async def httpx_status_error_handler(request: Request, exc: httpx.HTTPStatusError):
+    return JSONResponse(
+        status_code=exc.response.status_code,
+        content={"detail": f"External API error: {exc.response.text}"},
+    )
 
 
 if __name__ == "__main__":  # Точка входа для запуска приложения
