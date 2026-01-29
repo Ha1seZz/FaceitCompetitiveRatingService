4308b5965b8bd38a5180120ff887a09a297fb7e5
Ha1seZz <fahretdinovdamir7@gmail.com>
2026-01-30 01:06:39 +0500

    refactor(models): use player_id as primary key and fix steam_id type

--- app/models/player.py
+++ app/models/player.py
@@ -1,20 +1,19 @@
 from datetime import datetime
 
-from sqlalchemy import text
+from sqlalchemy import BigInteger, text
 from sqlalchemy.orm import Mapped, mapped_column
 
-from .mixins.id_int_pk import IdIntPkMixin
 from app.core.settings.base import Base
 
 
-class Player(IdIntPkMixin, Base):
-    nickname: Mapped[str] = mapped_column(nullable=False)
+class Player(Base):
+    nickname: Mapped[str] = mapped_column(unique=True, nullable=False)
     country: Mapped[str] = mapped_column(nullable=True)
     verified: Mapped[bool] = mapped_column(server_default=text("false"))
     steam_nickname: Mapped[str] = mapped_column(nullable=True)
-    steam_id_64: Mapped[int] = mapped_column(nullable=False)
+    steam_id_64: Mapped[int] = mapped_column(BigInteger, nullable=False)
     faceit_url: Mapped[str] = mapped_column(nullable=False)
-    player_id: Mapped[str] = mapped_column(nullable=False, unique=True)
+    player_id: Mapped[str] = mapped_column(primary_key=True)
     friends_count: Mapped[int] = mapped_column(server_default=text("0"))
     activated_at: Mapped[datetime] = mapped_column(nullable=False)
 
