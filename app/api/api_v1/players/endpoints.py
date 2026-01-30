775c77cdfc21b425be20084899e89b1b7cc5cee2
Ha1seZz <fahretdinovdamir7@gmail.com>
2026-01-30 19:49:22 +0500

    refactor(api): standardize player endpoints paths and naming

--- app/api/api_v1/players/endpoints.py
+++ app/api/api_v1/players/endpoints.py
@@ -1,11 +1,10 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from .crud import create_or_update_player
 from app.core.settings.db_helper import db_helper
 from .dependencies import get_current_faceit_player
-from .services.faceit_client import FaceitClient
-from .schemas import PlayerCreate, PlayerProfileDetails, PlayerCSStats
+from .schemas import PlayerProfileDetails, PlayerCSStats
 from app.core.config import settings
 
 
@@ -20,12 +19,12 @@ async def get_player_profile(
     player: dict = Depends(get_current_faceit_player),
     session: AsyncSession = Depends(db_helper.scoped_session_dependency),
 ):
-    """Получает данные об игроке из Faceit и сохраняет игрока в БД."""
+    """Получает данные об игроке из Faceit и синхронизирует игрока в БД."""
     await create_or_update_player(session=session, player_in=player)
     return player
 
 
-@router.get("/CSStats/{nickname}", response_model=PlayerCSStats)
-async def get_cs_stats(player: dict = Depends(get_current_faceit_player)):
-    """Получает CS статистику игрока на Faceit."""
+@router.get("/cs-stats/{nickname}", response_model=PlayerCSStats)
+async def get_player_cs_stats(player: dict = Depends(get_current_faceit_player)):
+    """Получает Counter-Strike статистику игрока на Faceit."""
     return player
